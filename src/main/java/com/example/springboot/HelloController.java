370180a5974b3cd8e9d06d8968f0372058d32571
amitmarwadi <amit.marwadi@gmail.com>
2021-11-10 02:55:08 -0600

    jenkins file

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@GetMapping("/springboottest")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
